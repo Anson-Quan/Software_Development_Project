dc0356072618581e474b6a17b79ea0dc04aef25b
Anson-Quan <ansonquan01@gmail.com>
2023-12-15 01:25:50 -0500

    Anson-commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +10,50 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB (make sure to replace the connection string with your MongoDB URI)
+mongoose.connect('mongodb://localhost:27017/software_project', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('error', (error) => console.error('MongoDB connection error:', error));
+mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
+
+// Define a User schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Middleware to parse JSON
+app.use(bodyParser.json());
+
+// Route to handle user registration
+app.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists!' });
+    }
+
+    // Create a new user
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully!' });
+  } catch (error) {
+    console.error('Error during registration:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,8 +63,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Static file handling
 app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
